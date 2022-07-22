a08df3812f537e2d3c1aa5c97a9489a2d489d5c7
Safayet <mrishafayet003@gmail.com>
2022-07-22 15:39:47 +0600

    banner complate

--- src/Components/Home/Banner/Banner.js
+++ src/Components/Home/Banner/Banner.js
@@ -1,28 +1,37 @@
 import React from 'react';
-import img1 from '../../../images/slider/slider-1.png';
-import img2 from '../../../images/slider/slider-2.png';
+import './Banner.css';
 
 const Banner = () => {
     return (
-        <div>
-            <div class="carousel w-full">
-                <div id="slide1" class="carousel-item relative w-full">
-                    <img src={img2} class="w-full" alt='' />
-                    <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                        <a href="#slide4" class="btn btn-circle">❮</a>
-                        <a href="#slide2" class="btn btn-circle">❯</a>
+        <div className="carousel w-full">
+            <div id="slide1" class="hero carousel-item relative banner-bg-1" >
+                <div class="hero-content text-neutral-content">
+                    <div class="max-w-md">
+                        <h1 class="mb-5 text-5xl">30% OFF</h1>
+                        <h1 class="mb-5 mr-14 text-4xl font-semibold text-black">FAVOURITE CLOTHING </h1>
+
+                        <button class="btn bg-white text-black hover:text-white">GET COLLECTION</button>
                     </div>
                 </div>
-                <div id="slide2" class="carousel-item relative w-full">
-                    <img src={img1} class="w-full" alt='' />
-                    <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                        <a href="#slide3" class="btn btn-circle">❮</a>
-                        <a href="#slide1" class="btn btn-circle">❯</a>
-                    </div>
-
-
+                <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                    <a href="#slide2" class="btn btn-circle">❮</a>
+                    <a href="#slide2" class="btn btn-circle">❯</a>
                 </div>
             </div>
+            <div id="slide2" class="hero min-h-screen carousel-item relative banner-bg-2" >
+                <div class="hero-content text-neutral-content">
+                    <div class="max-w-md">
+                        <h1 class="mb-14 text-5xl pr-28">30% OFF</h1>
+
+                        <button class="mt-14 btn bg-white text-black hover:text-white">GET COLLECTION</button>
+                    </div>
+                </div>
+                <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                    <a href="#slide1" class="btn btn-circle">❮</a>
+                    <a href="#slide1" class="btn btn-circle">❯</a>
+                </div>
+            </div>
+
         </div>
     );
 };
